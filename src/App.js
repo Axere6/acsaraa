13b96ad6d8a7d093023eb39d95a6ad67963cceeb
Axere6 <alexacsara6@gmail.com>
2023-07-18 18:24:06 -0500

    acsaraa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import C01componente from "./componentes/C01componente";
+import C02contador from "./componentes/C02contador";
+import C03doblecontador from "./componentes/C03doblecontador";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <C01componente/>
+
+      <hr/>
+      <C02contador/>
+
+      <hr/>
+      <C03doblecontador/>
+
+
+      
     </div>
   );
 }
